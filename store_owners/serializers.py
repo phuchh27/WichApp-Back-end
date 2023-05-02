aa2f7740c995462c850dc74d19881eb375490d75
phuchh27 <phuchh27@gmail.com>
2023-05-02 23:07:14 +0700

    create basic register

--- store_owners/serializers.py
+++ store_owners/serializers.py
@@ -1,20 +1,23 @@
 from rest_framework import serializers
 from backend.models import StoreOwners
-
+from django.core.mail import send_mail
+from django.conf import settings
 class StoreOwnersSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
     class Meta:
         model = StoreOwners
-        fields = ['email', 'phone', 'name', 'date_of_birth', 'password']
+        fields = ['username','email', 'phone', 'firstname', 'lastname', 'date_of_birth', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = StoreOwners.objects.create_user(
+            username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            name=validated_data['name'],
+            firstname=validated_data['firstname'],
+            lastname=validated_data['lastname'],
             phone=validated_data['phone'],
             date_of_birth=validated_data['date_of_birth']
         )
-        user.is_active = False  # set is_active to False
-        user.save()
+        
         return user
--- store_owners/views.py
+++ store_owners/views.py
@@ -1,18 +1,48 @@
-import secrets
-from django.conf import settings
-from rest_framework import status
+
+from rest_framework import status,generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+#from api.mixins import (StaffEditorPermissionMixin,UserQuerysetMixin)
 
 from backend.models import StoreOwners
 from .serializers import StoreOwnersSerializer
 from django.core.mail import send_mail
-@api_view(['POST'])
-def register_store_owner(request):
-    serializer = StoreOwnersSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from .utils import Utils
+
+
+class StoreOwnersRegisterAPIView(generics.ListCreateAPIView):
+    queryset = StoreOwners.objects.all()
+    serializer_class = StoreOwnersSerializer
+    def register_store_owner(self,request):
+        owner = request.data
+        serializer = self.serializer_class(data= owner)
+        if serializer.is_valid():
+            serializer.save()
+            owner_data = serializer.data
+            owner = StoreOwners.objects.get(email=owner_data['email'])
+            token = RefreshToken.for_user(owner).access_token
+            current_site = get_current_site(request).domain
+            relativeLink = reverse('email-verify')
+            
+            absurl = "http://" + current_site + relativeLink + "?token" + str(token)
+            
+            email_body = 'Hi' + owner.username + 'Use link below to verify your account\n'+ absurl
+            data = {'email_body': email_body,
+                    'to_email': owner.email, 
+                    'email_subject': 'Verify your email address'}
+            Utils.send_email(data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+store_owners_register_view = StoreOwnersRegisterAPIView.as_view()
+
+class VerifyEmail(generics.GenericAPIView):
+    def activate(self):
+        pass
+verify_email_view = VerifyEmail.as_view()
